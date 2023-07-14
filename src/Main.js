4ef0eb9bd20565f0c44402229ee9039dbea00076
ahmed osman <ahmedos099912@gmail.com>
2023-07-14 15:02:13 +0200

    adding main content

---
+++ src/Main.js
@@ -0,0 +1,39 @@
+import img from './/imgs/1-1.png';
+
+const Main = () => {
+    return ( <div className="maincontent">
+        <h2>Explore our Lastest Products</h2>
+        <div className="show">
+        <div className="card">
+            <img src={img} alt="" />
+            <h4>Nike Snikers</h4>
+            <p>High quailty shoes for men and women</p>
+            <div className="btns">
+                <button className='fav'>Favorite</button>
+                <button className='buy'>Buy Now</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img} alt="" />
+            <h4>Nike Snikers</h4>
+            <p>High quailty shoes for men and women</p>
+            <div className="btns">
+                <button className='fav'>Favorite</button>
+                <button className='buy'>Buy Now</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img} alt="" />
+            <h4>Nike Snikers</h4>
+            <p>High quailty shoes for men and women</p>
+            <div className="btns">
+                <button className='fav'>Favorite</button>
+                <button className='buy'>Buy Now</button>
+            </div>
+        </div>
+        </div>
+        
+    </div> );
+}
+ 
+export default Main;
---
+++ src/Navbar.js
@@ -0,0 +1,8 @@
+const Navbar = () => {
+    return ( <div className="nav">
+        <h4>E-Store</h4>
+        <input type="text" placeholder="Search"/>
+    </div> );
+}
+ 
+export default Navbar;
