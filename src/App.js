5f996b6001d47a02b93b6383a1f658a067803f54
ahmed osman <ahmedos099912@gmail.com>
2023-07-15 18:53:52 +0200

    adding the tabs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './Navbar'
 import Main from './Main';
+import Tabs from './Tabs';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Tabs></Tabs>
       <Main></Main>
     </div>
   );
---
+++ src/Tabs.js
@@ -0,0 +1,13 @@
+const Tabs = () => {
+    return ( <div className="tabs">
+        <ul>
+            <li className="active">Shoes</li>
+            <li>T-Shirts</li>
+            <li>Pants</li>
+            <li>Accessories</li>
+            <li>Glasses</li>
+        </ul>
+    </div> );
+}
+ 
+export default Tabs;
