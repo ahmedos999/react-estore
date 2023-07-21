a4649e7e8837311fa20b001598f2681039d9c791
ahmed osman <ahmedos099912@gmail.com>
2023-07-21 18:12:41 +0200

    icons style editing

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,15 @@
+import { BiCart } from 'react-icons/bi';
+//BsFillPersonFill
+import { BsFillPersonFill } from 'react-icons/bs';
+
 const Navbar = () => {
-    return ( <div className="nav">
+    return ( 
+    
+    <div className="nav">
         <h4>E-Store</h4>
-        <input type="text" placeholder="Search"/>
+        <div className="icons">
+        <BiCart className='icon'/><BsFillPersonFill className='icon'></BsFillPersonFill>
+        </div>
     </div> );
 }
  
