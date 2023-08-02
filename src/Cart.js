b39b85201ae83d61a3384648b48b4cee8651b2a5
ahmed osman <ahmedos099912@gmail.com>
2023-08-02 17:01:23 +0200

    cart card v-2

--- src/Cart.js
+++ src/Cart.js
@@ -1,6 +1,27 @@
+import { useState } from 'react';
 import img from './/imgs/S2.png'
+import {RiDeleteBin6Line} from 'react-icons/ri'
+
 
 const Cart = () => {
+    const [counter,setCounter] = useState(1);
+    function increment() {
+        //setCount(prevCount => prevCount+=1);
+        setCounter(function (prevCount) {
+          return (prevCount += 1);
+        });
+      }
+    
+      function decrement() {
+        setCounter(function (prevCount) {
+          if (prevCount > 0) {
+            return (prevCount -= 1); 
+          } else {
+            return (prevCount = 0);
+          }
+        });
+      }
+
     return ( <div className="shop">
     <div className="cart">
         <h2>Shopping Cart - 2 items</h2>
@@ -12,10 +33,19 @@ const Cart = () => {
             
             <h4>Size - M</h4>
             <h4>Color - Black</h4>
+            <h4>Pickup Style - In Store</h4>
+
+            <div className="counter">
+                <button onClick={decrement}>-</button>
+                <h4>{counter}</h4>
+                <button onClick={increment}>+</button>
+            </div>
             
             </div>
             <div className='options'>
-            <h4>Quantiy - 1</h4>
+            <div>
+                <RiDeleteBin6Line className='delete-icon'></RiDeleteBin6Line>
+            </div>
             <h3>Price - 20.00$</h3>
             </div>
 
