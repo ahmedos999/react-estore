f28b55c95d227c8521e4e5480cc2b2e3d8ed5f3f
ahmed osman <ahmedos099912@gmail.com>
2023-08-02 16:17:38 +0200

    cart card base

--- src/Cart.js
+++ src/Cart.js
@@ -3,13 +3,21 @@ import img from './/imgs/S2.png'
 const Cart = () => {
     return ( <div className="shop">
     <div className="cart">
-        <h4>Shopping Cart</h4>
-        <h4>2 items</h4>
+        <h2>Shopping Cart - 2 items</h2>
         <div className="cart-item">
             <img src={img} alt="" />
-            <h4>Nike shoes</h4>
-            <h6>Quantiy - 1</h6>
-            <h4>Price - 20.00$</h4>
+            <div className="cart-details">
+            <h2>Nike shoes</h2>
+            <p>high Quality Nike shoes</p>
+            
+            <h4>Size - M</h4>
+            <h4>Color - Black</h4>
+            
+            </div>
+            <div className='options'>
+            <h4>Quantiy - 1</h4>
+            <h3>Price - 20.00$</h3>
+            </div>
 
         </div>
     </div>
