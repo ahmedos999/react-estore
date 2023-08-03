6a2405c4f25ce0b3a5d27aee5e4c6c62af299040
ahmed osman <ahmedos099912@gmail.com>
2023-08-03 12:01:03 +0200

    order summary page

--- src/Cart.js
+++ src/Cart.js
@@ -50,6 +50,40 @@ const Cart = () => {
             </div>
 
         </div>
+        <div className="cart-item">
+            <img src={img} alt="" />
+            <div className="cart-details">
+            <h2>Nike shoes</h2>
+            <p>high Quality Nike shoes</p>
+            
+            <h4>Size - M</h4>
+            <h4>Color - Black</h4>
+            <h4>Pickup Style - In Store</h4>
+
+            <div className="counter">
+                <button onClick={decrement}>-</button>
+                <h4>{counter}</h4>
+                <button onClick={increment}>+</button>
+            </div>
+            
+            </div>
+            <div className='options'>
+            <div>
+                <RiDeleteBin6Line className='delete-icon'></RiDeleteBin6Line>
+            </div>
+            <h3>Price - 20.00$</h3>
+            </div>
+
+        </div>
+    </div>
+
+    <div className="payment-details">
+      <h2>Order Summary</h2>
+      <div className="row"><h6>Subtotal</h6> <h6>$20.00$</h6></div>
+      <div className="row"><p>Delivery</p><p>Free</p></div>
+      <div className="row"><p>VAT & TAX</p><p>0%</p></div>
+      <div className="row"><h6>Total</h6><h6>20.00$</h6></div>
+      <button className='checkout-btn'>Checkout</button>
     </div>
 </div> );
 }
