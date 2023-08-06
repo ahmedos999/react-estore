c3ac70b90bffffb3b02c2439cb1ad2b3ae5ac913
ahmed osman <ahmedos099912@gmail.com>
2023-08-06 22:34:17 +0200

    cart dyn img

--- src/Cart.js
+++ src/Cart.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import img from './/imgs/S2.png'
+// import img from './/imgs/S2.png'
 import {RiDeleteBin6Line} from 'react-icons/ri'
 import useFetch from './useFetch';
 
@@ -26,69 +26,51 @@ const Cart = () => {
       const {data,isPending,error} = useFetch('http://localhost:8000/cart')
 
     return ( <div className="shop">
-    <div className="cart">
-        <h2>Shopping Cart - 2 items</h2>
-        <div className="cart-item">
-            <img src={img} alt="" />
-            <div className="cart-details">
-            <h2>Nike shoes</h2>
-            <p>high Quality Nike shoes</p>
-            
-            <h4>Size - M</h4>
-            <h4>Color - Black</h4>
-            <h4>Pickup Style - In Store</h4>
+      {/* <h2>Shopping Cart - 2 items</h2> */}
+      {isPending && <div>Loading...</div>}
+      {error && <div> {error} </div>}
+       {data && data.map((data)=>(
+          <div className="cart">
+          
 
-            <div className="counter">
-                <button onClick={decrement}>-</button>
-                <h4>{counter}</h4>
-                <button onClick={increment}>+</button>
-            </div>
-            
-            </div>
-            <div className='options'>
-            <div>
-                <RiDeleteBin6Line className='delete-icon'></RiDeleteBin6Line>
-            </div>
-            <h3>Price - 20.00$</h3>
-            </div>
-
-        </div>
-        <div className="cart-item">
-            <img src={img} alt="" />
-            <div className="cart-details">
-            <h2>Nike shoes</h2>
-            <p>high Quality Nike shoes</p>
-            
-            <h4>Size - M</h4>
-            <h4>Color - Black</h4>
-            <h4>Pickup Style - In Store</h4>
-
-            <div className="counter">
-                <button onClick={decrement}>-</button>
-                <h4>{counter}</h4>
-                <button onClick={increment}>+</button>
-            </div>
-            
-            </div>
-            <div className='options'>
-            <div>
-                <RiDeleteBin6Line className='delete-icon'></RiDeleteBin6Line>
-            </div>
-            <h3>Price - 20.00$</h3>
-            </div>
-
-        </div>
-    </div>
-
-    <div className="payment-details">
-      <h2>Order Summary</h2>
-      <div className="row"><h6>Subtotal</h6> <h6>$20.00$</h6></div>
-      <div className="row"><p>Delivery</p><p>Free</p></div>
-      <div className="row"><p>VAT & TAX</p><p>0%</p></div>
-      <div className="row"><h6>Total</h6><h6>20.00$</h6></div>
-      <button className='checkout-btn'>Checkout</button>
-    </div>
-</div> );
+              <div className="cart-item">
+              <img src={require(`${data.img}`)} alt="" />
+                  <div className="cart-details">
+                  <h2>{data.name}</h2>
+                  <p>{data.description}</p>
+                  
+                  <h4>Size - M</h4>
+                  <h4>Color - Black</h4>
+                  <h4>Pickup Style - In Store</h4>
+      
+                  <div className="counter">
+                      <button onClick={decrement}>-</button>
+                      <h4>{counter}</h4>
+                      <button onClick={increment}>+</button>
+                  </div>
+                  
+                  </div>
+                  <div className='options'>
+                  <div>
+                      <RiDeleteBin6Line className='delete-icon'></RiDeleteBin6Line>
+                  </div>
+                  <h3>Price - 20.00$</h3>
+                  </div>
+      
+              </div>
+          </div>
+      
+          
+      ))}
+      <div className="payment-details">
+            <h2>Order Summary</h2>
+            <div className="row"><h6>Subtotal</h6> <h6>$20.00$</h6></div>
+            <div className="row"><p>Delivery</p><p>Free</p></div>
+            <div className="row"><p>VAT & TAX</p><p>0%</p></div>
+            <div className="row"><h6>Total</h6><h6>20.00$</h6></div>
+            <button className='checkout-btn'>Checkout</button>
+          </div>
+    </div>);
 }
  
 export default Cart;
