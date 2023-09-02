67092bad13610261077777cc3664780b87fd04a1
ahmed osman <ahmedos099912@gmail.com>
2023-09-02 09:40:41 +0200

    home page link

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = (data) => {
     console.log(data)
     return ( 
     <div className="nav">
-        <h4>E-Store</h4>
+        <Link to="/"><h4>E-Store</h4></Link>
         <div className="icons">
         <Link to="/Cart"><div className="badge-2"><BiCart className='icon'/>{data.data&&<div className="badgenumber">{data.data.length}</div>}</div></Link>
         <Link to="/Favorite"><MdFavoriteBorder className='icon'></MdFavoriteBorder></Link>
