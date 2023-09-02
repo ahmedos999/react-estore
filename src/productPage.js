2abf73ca6aae41813f7b1477db18bba049adada7
ahmed osman <ahmedos099912@gmail.com>
2023-09-02 12:22:51 +0200

    add color and size productpage

--- src/productPage.js
+++ src/productPage.js
@@ -39,10 +39,11 @@ const Product = (props) => {
         <h2>{shoe.name} <span><SiNike className='nike'></SiNike></span></h2>
         <p>This shoes are prefect for running and training they give you comfortable space for you foot, it also has good shoe floor that prevent foot from sweating.</p>
         <div className="rate"><BsStarFill className='rating-icon'/><BsStarFill className='rating-icon'/><BsStarFill className='rating-icon'/><BsStarFill className='rating-icon'/><BsStarHalf className='rating-icon'/></div>
-        <div className="original-price"><span>{shoe.price/2}$</span>{shoe.price}$</div>
+        <div className="item-size"><h5>Size - M</h5><h5>Color - gray</h5></div>
+        <div className="original-price">Price: <span>{shoe.price/2}$</span>{shoe.price}$</div>
 
         <div className="extra-info">
-            <div className="delivery"><h4>Delivery Time</h4><p>2 Days</p></div>
+            <div className="delivery"><h4>Delivery Time</h4><p>Not available</p></div>
             <div className="delivery"><h4>Product Price</h4><p>{shoe.price}$</p></div>
             <div className="delivery"><h4>VAT and TAX</h4><p>{shoe.price * 15/100}$</p></div>
             <div className="Total"><h4>Total amount</h4><p>{shoe.price+shoe.price * 15/100}$</p></div>
