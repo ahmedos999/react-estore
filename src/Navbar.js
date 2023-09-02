c460a575f437e3bb0e2d8aa4284a53a115075756
ahmed osman <ahmedos099912@gmail.com>
2023-09-02 09:35:38 +0200

    deleted profile icon

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,5 @@
 import { BiCart } from 'react-icons/bi';
 //BsFillPersonFill
-import { BsFillPersonFill } from 'react-icons/bs';
 import {Link} from 'react-router-dom';
 import {MdFavoriteBorder} from "react-icons/md"
 
@@ -15,7 +14,6 @@ const Navbar = (data) => {
         <div className="icons">
         <Link to="/Cart"><div className="badge-2"><BiCart className='icon'/>{data.data&&<div className="badgenumber">{data.data.length}</div>}</div></Link>
         <Link to="/Favorite"><MdFavoriteBorder className='icon'></MdFavoriteBorder></Link>
-        <BsFillPersonFill className='icon'></BsFillPersonFill>
         </div>
     </div> );
 }
